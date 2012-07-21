bef38d842d6b715b8e9005519f765f11c421d6c3
Mike BRIGHT <xbmc_PBell.git@mjbright.net>
2012-07-21 17:39:12 +0200

    Improved datehandling.     Old copies stored on basis of date (YYYY_MM_DD) for month,week,day periods or hour (YYY_MM_DD_HHh)
    Externalized Scraper config to module Scraper_config
    
    Added more e-mail options
    
    Added TODO.txt file
    
    Bug fixes
    
    Added generalized get_subtree_from_html to search on tag/atrrib=val (for root_div_class,root_div_id for the moment)
       => Todo: cleanup exception handling
    
    Added better unicode/binary file handling.
       => Todo: It's a real hack ... to be understood/cleaned up!!
    
    Todo: Check last run log for problems ...

---
+++ Scraper_config.py
@@ -0,0 +1,19 @@
+
+Scraper_config = {
+    'SEND_TO'     : 'scraper@mjbright.net',
+
+    'SENDER_EMAIL': 'scraper_cron@mjbright.net',
+    'SENDER_NAME' : 'Scraper',
+
+    'SMTP_HOST' : 'smtp.free.fr',
+
+    'SEND_MAIL_MIN_BYTES'  : 30,
+
+    'SEND_MAIL_INDIVIDUAL' : True,
+    'SEND_MAIL_GLOBAL'     : False,
+
+    'SEND_ERROR_MAIL_INDIVIDUAL' : False,
+    'SEND_ERROR_MAIL_GLOBAL'     : False,
+    'SEND_MAIL_SUMMARY'          : False,
+}
+
